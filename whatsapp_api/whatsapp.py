f2ee52a57c4569fda3cd5d64ab0fbaa99ed3ad87
Victor Rivas <victorenrique2002@gmail.com>
2024-12-11 01:45:34 -0400

    First commit

---
+++ whatsapp_api/whatsapp.py
@@ -0,0 +1,9 @@
+
+class Whatsapp:
+    def __init__(self, message):
+        self.message = message
+
+    def send_message(self):
+        print(f"Message sent: {self.message}")
+        
+        
